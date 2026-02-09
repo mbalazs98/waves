6acf2ccbdec27beebe9ed9d1000adc909241c3f9
mbalazs98 <49280623+mbalazs98@users.noreply.github.com>
2026-02-09 17:05:53 +0000

    Refactor neuron population and synapse initialization

--- test_genn.py
+++ test_genn.py
@@ -10,10 +10,13 @@ model.dt = 0.1
 lif_params = {"C": 0.2, "TauM": 20.0, "Vrest": -65.0, "Vreset": -70.0,
               "Vthresh": -50.0, "Ioffset": 0.0, "TauRefrac": 5.0}
 
+poisson_params = {"rate": 20.0}
+
+
 
 lif_init = {"V": init_var("Normal", {"mean": -68.0901, "sd": 5.7296}),
             "RefracTime": 0.0}
-
+poisson_init = {"timeStepToSpike": 0.0}
 
 
 
@@ -25,21 +28,18 @@ sigma = 400.0
 K = 3000
 exc_num = int(all_neuron * exc_ratio)
 inh_num = int(all_neuron * inh_ratio)
+input_num = 10 * 10
 L = 6000.0
 
 
 n_side_E = int(np.ceil(np.sqrt(exc_num)))
 n_side_I = int(np.ceil(np.sqrt(inh_num)))
 
-EE_dist = L/n_side_E
 
 
-
-
-
-fixed_number_post = create_sparse_connect_init_snippet(
-    "fixed_number_post",
-    params=[("num", "unsigned int"), ("sigma_space", "float"), ("grid_num_x", "unsigned int")],
+input_to_hid = create_sparse_connect_init_snippet(
+    "input_to_hid",
+    params=[("num", "unsigned int"), ("loc", "int"), ("grid_num_x", "unsigned int")],
     row_build_code=
         """
         const int xPre = id_pre % grid_num_x;
@@ -60,19 +60,44 @@ fixed_number_post = create_sparse_connect_init_snippet(
         """,   
         calc_max_row_len_func=lambda num_pre, num_post, pars: pars["num"])
 
-fixed_number_post_resize = create_sparse_connect_init_snippet(
-    "fixed_number_post_resize",
-    params=[("num", "unsigned int"), ("sigma_space", "float"), ("grid_num_x", "unsigned int"), ("grid_num_x2", "unsigned int")],
+
+fixed_number_post = create_sparse_connect_init_snippet(
+    "fixed_number_post",
+    params=[("num", "unsigned int"), ("sigma_space", "float"), ("grid_num_x", "unsigned int")],
     row_build_code=
         """
         const int xPre = id_pre % grid_num_x;
         const int yPre = id_pre / grid_num_x;
         int count = num;
         while(count > 0) {
-            const float distanceX = gennrand_normal() * sigma_space;
-            const float distanceY = gennrand_normal() * sigma_space;
-            int xPost = (int)round((xPre + distanceX)/grid_num_x/grid_num_x2);
-            int yPost = (int)round((yPre + distanceX)/grid_num_x/grid_num_x2);
+            const int distanceX = (int)round(gennrand_normal() * sigma_space);
+            const int distanceY = (int)round(gennrand_normal() * sigma_space);
+            int xPost = xPre + distanceX;
+            int yPost = yPre + distanceY;
+            if((distanceX == 0 && distanceY == 0) || (xPost < 0 || xPost >= grid_num_x || yPost < 0 || yPost >= grid_num_x)){
+                continue;
+            }
+            count--;
+            const int id_post = (yPost * grid_num_x) + xPost;
+            addSynapse(id_post);
+        }
+        """,   
+        calc_max_row_len_func=lambda num_pre, num_post, pars: pars["num"])
+
+fixed_number_post_resize = create_sparse_connect_init_snippet(
+    "fixed_number_post_resize",
+    params=[("num", "unsigned int"), ("sigma_space", "float"), ("grid_num_x", "unsigned int"), ("grid_num_x2", "unsigned int")],
+    row_build_code=
+        """
+        const float ratio = (float)grid_num_x2 / (float)grid_num_x;
+        const int xPre = (id_pre % grid_num_x) * ratio;
+        const int yPre = (id_pre / grid_num_x) * ratio;
+        int count = num;
+        while(count > 0) {
+            const int distanceX = (int)round(gennrand_normal() * sigma_space);
+            const int distanceY = (int)round(gennrand_normal() * sigma_space);
+            int xPost = xPre + distanceX;
+            int yPost = yPre + distanceY;
             if((xPost < 0 || xPost >= grid_num_x2 || yPost < 0 || yPost >= grid_num_x2)){
                 continue;
             }
@@ -84,7 +109,7 @@ fixed_number_post_resize = create_sparse_connect_init_snippet(
         calc_max_row_len_func=lambda num_pre, num_post, pars: pars["num"])
 
 
-
+input_pop = model.add_neuron_population("Input", input_num, "Poisson", poisson_params, poisson_init)
 exc_pop = model.add_neuron_population("E", exc_num, "LIF", lif_params, lif_init)
 inh_pop = model.add_neuron_population("I", inh_num, "LIF", lif_params, lif_init)
 
@@ -92,16 +117,26 @@ exc_pop.spike_recording_enabled = True
 inh_pop.spike_recording_enabled = True
 
 
-conduction_delay = 0.2 #mm/ms
 
 exc_synapse_init = {"g": 0.001}
-inh_synapse_init = {"g": -0.01}
+inh_synapse_init = {"g": 0.01}
 
 
 exc_post_syn_params = {"tau": 5.0, "E": 0.0}
 inh_post_syn_params = {"tau": 5.0, "E": -80.0}
 
 
+InputE_syn_pop = model.add_synapse_population("InputE", "SPARSE",
+    input_pop, exc_pop,
+    init_weight_update("StaticPulseConstantWeight", {"g": 0.5}),
+    init_postsynaptic("ExpCurr", {"tau": 5.0}),
+    init_sparse_connectivity("FixedProbability", {"prob": 100/exc_num*exc_ratio}))
+
+InputI_syn_pop = model.add_synapse_population("InputI", "SPARSE",
+    input_pop, inh_pop,
+    init_weight_update("StaticPulseConstantWeight", {"g": 0.5}),
+    init_postsynaptic("ExpCurr", {"tau": 5.0}),
+    init_sparse_connectivity("FixedProbability", {"prob": 100/inh_num*inh_ratio}))
 
 
 
@@ -110,6 +145,7 @@ EE_syn_pop = model.add_synapse_population("EE", "SPARSE",
     init_weight_update("StaticPulseConstantWeight", exc_synapse_init),
     #init_weight_update("StaticPulseDendriticDelay", EE_synapse_init),
     init_postsynaptic("ExpCond", exc_post_syn_params, var_refs={"V": create_var_ref(exc_pop, "V")}),
+    #init_sparse_connectivity("FixedNumberPostWithReplacement", {"num": K*exc_ratio}))
     init_sparse_connectivity(fixed_number_post, {"num": int(K*exc_ratio), "sigma_space": sigma/L*n_side_E, "grid_num_x": int(n_side_E)}))
 
 
@@ -117,19 +153,27 @@ EE_syn_pop = model.add_synapse_population("EE", "SPARSE",
 EI_syn_pop = model.add_synapse_population("EI", "SPARSE",
     exc_pop, inh_pop,
     init_weight_update("StaticPulseConstantWeight", exc_synapse_init),
+    #init_postsynaptic("ExpCond", inh_post_syn_params, var_refs={"V": create_var_ref(inh_pop, "V")}),
     init_postsynaptic("ExpCond", exc_post_syn_params, var_refs={"V": create_var_ref(inh_pop, "V")}),
+    #init_sparse_connectivity("FixedNumberPostWithReplacement",{"num": K*inh_ratio}))
+
     init_sparse_connectivity(fixed_number_post_resize, {"num": int(K*inh_ratio), "sigma_space": sigma/L*n_side_I, "grid_num_x": int(n_side_E), "grid_num_x2": int(n_side_I)}))
 
 II_syn_pop = model.add_synapse_population("II", "SPARSE",
     inh_pop, inh_pop,
     init_weight_update("StaticPulseConstantWeight", inh_synapse_init),
     init_postsynaptic("ExpCond", inh_post_syn_params, var_refs={"V": create_var_ref(inh_pop, "V")}),
+    #init_sparse_connectivity("FixedNumberPostWithReplacement", {"num": K*inh_ratio}))
+
     init_sparse_connectivity(fixed_number_post, {"num": int(K*inh_ratio), "sigma_space": sigma/L*n_side_I, "grid_num_x": int(n_side_I)}))
 
 IE_syn_pop = model.add_synapse_population("IE", "SPARSE",
     inh_pop, exc_pop,
     init_weight_update("StaticPulseConstantWeight", inh_synapse_init),
+    #init_postsynaptic("ExpCond", exc_post_syn_params, var_refs={"V": create_var_ref(exc_pop, "V")}),
     init_postsynaptic("ExpCond", inh_post_syn_params, var_refs={"V": create_var_ref(exc_pop, "V")}),
+    #init_sparse_connectivity("FixedNumberPostWithReplacement", {"num": K*exc_ratio}))
+
     init_sparse_connectivity(fixed_number_post_resize, {"num": int(K*exc_ratio), "sigma_space": sigma/L*n_side_E, "grid_num_x": int(n_side_I), "grid_num_x2": int(n_side_E)}))
 
 
@@ -137,7 +181,6 @@ model.build()
 
 
 model.load(num_recording_timesteps=10000)
-
 
 while model.timestep < 10000:
     model.step_time()
