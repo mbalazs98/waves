4d91adbd69b362a0196d7020bfd4b8036391bc4d
mbalazs98 <49280623+mbalazs98@users.noreply.github.com>
2026-02-11 11:32:31 +0000

    Refactor neuron model and update synapse populations
    Refactor neuron model parameters and initialization. Update synapse populations and spike recording for excitatory and inhibitory neurons.

--- test_genn.py
+++ test_genn.py
@@ -1,64 +1,50 @@
-
 import numpy as np
 import matplotlib.pyplot as plt
+from time import time
 
-from pygenn import GeNNModel, init_postsynaptic, init_sparse_connectivity, init_var, init_weight_update, create_var_ref, create_sparse_connect_init_snippet, create_var_init_snippet
+from pygenn import (GeNNModel, VarLocation, init_var,
+                    init_postsynaptic, init_weight_update, create_sparse_connect_init_snippet, create_var_init_snippet,
+                    init_sparse_connectivity)
 
-model = GeNNModel("float", "tutorial_wave")
-model.dt = 0.1
+# Parameters
+TIMESTEP = 1.0
+NUM_NEURONS = 800000#1_012_500
 
-lif_params = {"C": 0.2, "TauM": 20.0, "Vrest": -65.0, "Vreset": -70.0,
-              "Vthresh": -50.0, "Ioffset": 0.0, "TauRefrac": 5.0}
+RESET_VOLTAGE = -60.0
+THRESHOHOLD_VOLTAGE = -50.0
 
-poisson_params = {"rate": 20.0}
-
-
-
-lif_init = {"V": init_var("Normal", {"mean": -68.0901, "sd": 5.7296}),
-            "RefracTime": 0.0}
-poisson_init = {"timeStepToSpike": 0.0}
-
-
-
-
-exc_ratio = 0.8
-inh_ratio = 0.2
-all_neuron = 1_012_500
-sigma = 400.0
 K = 3000
-exc_num = int(all_neuron * exc_ratio)
-inh_num = int(all_neuron * inh_ratio)
-input_num = 10 * 10
-L = 6000.0
+
+PROBABILITY_CONNECTION = K/NUM_NEURONS#0.1
+
+EXCITATORY_INHIBITORY_RATIO = 4.0
+
+NUM_EXCITATORY = int(round((NUM_NEURONS * EXCITATORY_INHIBITORY_RATIO) / (1.0 + EXCITATORY_INHIBITORY_RATIO)))
+
+NUM_INHIBITORY = NUM_NEURONS - NUM_EXCITATORY
+SCALE = (4000.0 / NUM_NEURONS) * (0.02 / PROBABILITY_CONNECTION)
+
+EXCITATORY_WEIGHT = 4.0E-3 * SCALE
+INHIBITORY_WEIGHT = -51.0E-3 * SCALE
+
+model = GeNNModel("float", "va_benchmark")
+model.dt = TIMESTEP
+model.default_narrow_sparse_ind_enabled = True
+fixed_prob = {"prob": PROBABILITY_CONNECTION}
+
+lif_params = {"C": 1.0, "TauM": 20.0, "Vrest": -49.0, "Vreset": RESET_VOLTAGE,
+              "Vthresh": THRESHOHOLD_VOLTAGE, "Ioffset": 0.0, "TauRefrac": 5.0}
+
+lif_init = {"V": init_var("Uniform", {"min": RESET_VOLTAGE, "max": THRESHOHOLD_VOLTAGE}),
+            "RefracTime": 0.0}
+
+excitatory_pop = model.add_neuron_population("E", NUM_EXCITATORY, "LIF", lif_params, lif_init)
+inhibitory_pop = model.add_neuron_population("I", NUM_INHIBITORY, "LIF", lif_params, lif_init)
+
+excitatory_pop.spike_recording_enabled = True
+inhibitory_pop.spike_recording_enabled = True
 
 
-n_side_E = int(np.ceil(np.sqrt(exc_num)))
-n_side_I = int(np.ceil(np.sqrt(inh_num)))
-
-
-
-input_to_hid = create_sparse_connect_init_snippet(
-    "input_to_hid",
-    params=[("num", "unsigned int"), ("loc", "int"), ("grid_num_x", "unsigned int")],
-    row_build_code=
-        """
-        const int xPre = id_pre % grid_num_x;
-        const int yPre = id_pre / grid_num_x;
-        int count = num;
-        while(count > 0) {
-            const int distanceX = (int)round(gennrand_normal() * sigma_space);
-            const int distanceY = (int)round(gennrand_normal() * sigma_space);
-            int xPost = xPre + distanceX;
-            int yPost = yPre + distanceY;
-            if((distanceX == 0 && distanceY == 0) || (xPost < 0 || xPost >= grid_num_x || yPost < 0 || yPost >= grid_num_x)){
-                continue;
-             }
-            count--;
-            const int id_post = (yPost * grid_num_x) + xPost;
-            addSynapse(id_post);
-        }
-        """,   
-        calc_max_row_len_func=lambda num_pre, num_post, pars: pars["num"])
 
 
 fixed_number_post = create_sparse_connect_init_snippet(
@@ -109,107 +95,129 @@ fixed_number_post_resize = create_sparse_connect_init_snippet(
         calc_max_row_len_func=lambda num_pre, num_post, pars: pars["num"])
 
 
-input_pop = model.add_neuron_population("Input", input_num, "Poisson", poisson_params, poisson_init)
-exc_pop = model.add_neuron_population("E", exc_num, "LIF", lif_params, lif_init)
-inh_pop = model.add_neuron_population("I", inh_num, "LIF", lif_params, lif_init)
+calc_dist = create_var_init_snippet(
+    "calc_dist",
+    
+    params=[("delay", "float"),("grid_num_x", "unsigned int")],
+    var_init_code=
+        """
+        const float xPre = id_pre % grid_num_x;
+        const float yPre = id_pre / grid_num_x;
+        const float xPost = id_post % grid_num_x;
+        const float yPost = id_post / grid_num_x;
+        float dist = (float)sqrt(pow(xPre - xPost, 2) + pow(yPre - yPost, 2));
+        value = dist * delay;
+        """
+    )
 
-exc_pop.spike_recording_enabled = True
-inh_pop.spike_recording_enabled = True
+calc_dist_resize = create_var_init_snippet(
+    "calc_dist_resize",
+    
+    params=[("delay", "float"),("grid_num_x", "unsigned int"), ("grid_num_x2", "unsigned int")],
+    var_init_code=
+        """
+        const float ratio = (float)grid_num_x2 / (float)grid_num_x;
+        const int xPre = (id_pre % grid_num_x) * ratio;
+        const int yPre = (id_pre / grid_num_x) * ratio;
+        const float xPost = id_post % grid_num_x2;
+        const float yPost = id_post / grid_num_x2;
+        float dist = (float)sqrt(pow(xPre - xPost, 2) + pow(yPre - yPost, 2));
+        value = dist * delay;
+        """
+    )
 
+L = NUM_NEURONS/1012500*6000 #6000.0
+sigma = NUM_NEURONS/1012500*400.0 #400.0
+n_side_E = int(np.ceil(np.sqrt(NUM_EXCITATORY)))
+n_side_I = int(np.ceil(np.sqrt(NUM_INHIBITORY)))
+EE_dist = L/n_side_E
+II_dist = L/n_side_I
 
+conduction_delay = 0.2 #mm/ms
+EE_synapse_init = init_weight_update("StaticPulseDendriticDelay", {}, {"g": EXCITATORY_WEIGHT, "d": init_var(calc_dist, {"delay": EE_dist*conduction_delay, "grid_num_x": int(n_side_E)})})
+EI_synapse_init = init_weight_update("StaticPulseDendriticDelay", {}, {"g": EXCITATORY_WEIGHT, "d": init_var(calc_dist_resize, {"delay": II_dist*conduction_delay, "grid_num_x": int(n_side_E), "grid_num_x2": int(n_side_I)})})
+II_synapse_init = init_weight_update("StaticPulseDendriticDelay", {}, {"g": INHIBITORY_WEIGHT, "d": init_var(calc_dist, {"delay": II_dist*conduction_delay, "grid_num_x": int(n_side_I)})})
+IE_synapse_init = init_weight_update("StaticPulseDendriticDelay", {}, {"g": INHIBITORY_WEIGHT, "d": init_var(calc_dist_resize, {"delay": EE_dist*conduction_delay, "grid_num_x": int(n_side_I), "grid_num_x2": int(n_side_E)})})
 
-exc_synapse_init = {"g": 0.001}
-inh_synapse_init = {"g": 0.01}
-
-
-exc_post_syn_params = {"tau": 5.0, "E": 0.0}
-inh_post_syn_params = {"tau": 5.0, "E": -80.0}
-
-
-InputE_syn_pop = model.add_synapse_population("InputE", "SPARSE",
-    input_pop, exc_pop,
-    init_weight_update("StaticPulseConstantWeight", {"g": 0.5}),
-    init_postsynaptic("ExpCurr", {"tau": 5.0}),
-    init_sparse_connectivity("FixedProbability", {"prob": 100/exc_num*exc_ratio}))
-
-InputI_syn_pop = model.add_synapse_population("InputI", "SPARSE",
-    input_pop, inh_pop,
-    init_weight_update("StaticPulseConstantWeight", {"g": 0.5}),
-    init_postsynaptic("ExpCurr", {"tau": 5.0}),
-    init_sparse_connectivity("FixedProbability", {"prob": 100/inh_num*inh_ratio}))
-
+#excitatory_weight_init = init_weight_update("StaticPulseConstantWeight", {"g": EXCITATORY_WEIGHT})
+#inhibitory_weight_init = init_weight_update("StaticPulseConstantWeight", {"g": INHIBITORY_WEIGHT})
+excitatory_postsynaptic_init = init_postsynaptic("ExpCurr", {"tau": 5.0})
+inhibitory_postsynaptic_init = init_postsynaptic("ExpCurr", {"tau": 10.0})
 
 
 EE_syn_pop = model.add_synapse_population("EE", "SPARSE",
-    exc_pop, exc_pop,
-    init_weight_update("StaticPulseConstantWeight", exc_synapse_init),
-    #init_weight_update("StaticPulseDendriticDelay", EE_synapse_init),
-    init_postsynaptic("ExpCond", exc_post_syn_params, var_refs={"V": create_var_ref(exc_pop, "V")}),
-    #init_sparse_connectivity("FixedNumberPostWithReplacement", {"num": K*exc_ratio}))
-    init_sparse_connectivity(fixed_number_post, {"num": int(K*exc_ratio), "sigma_space": sigma/L*n_side_E, "grid_num_x": int(n_side_E)}))
-
-
+    excitatory_pop, excitatory_pop,
+    #excitatory_weight_init, excitatory_postsynaptic_init,
+    EE_synapse_init, excitatory_postsynaptic_init,
+    #init_sparse_connectivity("FixedProbabilityNoAutapse", fixed_prob))
+    init_sparse_connectivity(fixed_number_post, {"num": int(K*EXCITATORY_INHIBITORY_RATIO/(1+EXCITATORY_INHIBITORY_RATIO)), "sigma_space": sigma/L*n_side_E, "grid_num_x": int(n_side_E)}))
 
 EI_syn_pop = model.add_synapse_population("EI", "SPARSE",
-    exc_pop, inh_pop,
-    init_weight_update("StaticPulseConstantWeight", exc_synapse_init),
-    #init_postsynaptic("ExpCond", inh_post_syn_params, var_refs={"V": create_var_ref(inh_pop, "V")}),
-    init_postsynaptic("ExpCond", exc_post_syn_params, var_refs={"V": create_var_ref(inh_pop, "V")}),
-    #init_sparse_connectivity("FixedNumberPostWithReplacement",{"num": K*inh_ratio}))
-
-    init_sparse_connectivity(fixed_number_post_resize, {"num": int(K*inh_ratio), "sigma_space": sigma/L*n_side_I, "grid_num_x": int(n_side_E), "grid_num_x2": int(n_side_I)}))
+    excitatory_pop, inhibitory_pop,
+    #excitatory_weight_init, excitatory_postsynaptic_init,
+    EI_synapse_init, excitatory_postsynaptic_init,
+    #init_sparse_connectivity("FixedProbability", fixed_prob))
+    #init_sparse_connectivity("FixedNumberPostWithReplacement", {"num": 3000*0.2}))
+    init_sparse_connectivity(fixed_number_post_resize, {"num": int(K*(1-(EXCITATORY_INHIBITORY_RATIO/(1+EXCITATORY_INHIBITORY_RATIO)))), "sigma_space": sigma/L*n_side_I, "grid_num_x": int(n_side_E), "grid_num_x2": int(n_side_I)}))
 
 II_syn_pop = model.add_synapse_population("II", "SPARSE",
-    inh_pop, inh_pop,
-    init_weight_update("StaticPulseConstantWeight", inh_synapse_init),
-    init_postsynaptic("ExpCond", inh_post_syn_params, var_refs={"V": create_var_ref(inh_pop, "V")}),
-    #init_sparse_connectivity("FixedNumberPostWithReplacement", {"num": K*inh_ratio}))
-
-    init_sparse_connectivity(fixed_number_post, {"num": int(K*inh_ratio), "sigma_space": sigma/L*n_side_I, "grid_num_x": int(n_side_I)}))
+    inhibitory_pop, inhibitory_pop,
+    #inhibitory_weight_init, inhibitory_postsynaptic_init,
+    II_synapse_init, inhibitory_postsynaptic_init,
+    #init_sparse_connectivity("FixedProbabilityNoAutapse", fixed_prob))
+    #init_sparse_connectivity("FixedNumberPostWithReplacement", {"num": 3000*0.2}))
+    init_sparse_connectivity(fixed_number_post, {"num": int(K*(1-(EXCITATORY_INHIBITORY_RATIO/(1+EXCITATORY_INHIBITORY_RATIO)))), "sigma_space": sigma/L*n_side_I, "grid_num_x": int(n_side_I)}))
+    
 
 IE_syn_pop = model.add_synapse_population("IE", "SPARSE",
-    inh_pop, exc_pop,
-    init_weight_update("StaticPulseConstantWeight", inh_synapse_init),
-    #init_postsynaptic("ExpCond", exc_post_syn_params, var_refs={"V": create_var_ref(exc_pop, "V")}),
-    init_postsynaptic("ExpCond", inh_post_syn_params, var_refs={"V": create_var_ref(exc_pop, "V")}),
-    #init_sparse_connectivity("FixedNumberPostWithReplacement", {"num": K*exc_ratio}))
+    inhibitory_pop, excitatory_pop,
+    #inhibitory_weight_init, inhibitory_postsynaptic_init,
+    IE_synapse_init, inhibitory_postsynaptic_init,
+    #init_sparse_connectivity("FixedProbability", fixed_prob))
+    #init_sparse_connectivity("FixedNumberPostWithReplacement", {"num": 3000*0.8}))
+    init_sparse_connectivity(fixed_number_post_resize, {"num": int(K*EXCITATORY_INHIBITORY_RATIO/(1+EXCITATORY_INHIBITORY_RATIO)), "sigma_space": sigma/L*n_side_E, "grid_num_x": int(n_side_I), "grid_num_x2": int(n_side_E)}))
 
-    init_sparse_connectivity(fixed_number_post_resize, {"num": int(K*exc_ratio), "sigma_space": sigma/L*n_side_E, "grid_num_x": int(n_side_I), "grid_num_x2": int(n_side_E)}))
-
-
+EE_syn_pop.max_dendritic_delay_timesteps = int((2*(400**2))**0.5 * (conduction_delay / model.dt))
+EI_syn_pop.max_dendritic_delay_timesteps = int((2*(400**2))**0.5 * (conduction_delay / model.dt))
+II_syn_pop.max_dendritic_delay_timesteps = int((2*(400**2))**0.5 * (conduction_delay / model.dt))
+IE_syn_pop.max_dendritic_delay_timesteps = int((2*(400**2))**0.5 * (conduction_delay / model.dt))
+print("Building Model")
 model.build()
-
-
+print("Loading Model")
 model.load(num_recording_timesteps=10000)
 
+sim_start_time = time()
 while model.timestep < 10000:
     model.step_time()
 
+sim_end_time = time()
+print("Simulation time:%fs" % (sim_end_time - sim_start_time))
 
+# Download recording data
 model.pull_recording_buffers_from_device()
 
-exc_spike_times, exc_spike_ids = exc_pop.spike_recording_data[0]
-inh_spike_times, inh_spike_ids = inh_pop.spike_recording_data[0]
+exc_spike_times, exc_spike_ids = excitatory_pop.spike_recording_data[0]
+inh_spike_times, inh_spike_ids = inhibitory_pop.spike_recording_data[0]
 
 
 fig, axes = plt.subplots(3, sharex=True, figsize=(20, 10))
 
 # Define some bins to calculate spike rates
-bin_size = 20.0
+bin_size = 10.0
 rate_bins = np.arange(0, 1000.0, bin_size)
 rate_bin_centres = rate_bins[:-1] + (bin_size / 2.0)
 
 # Plot excitatory and inhibitory spikes on first axis
-axes[0].scatter(exc_spike_times, exc_spike_ids, s=1)
-axes[0].scatter(inh_spike_times, inh_spike_ids + exc_num, s=1)
+axes[0].scatter(exc_spike_times, exc_spike_ids, s=0.01)
+axes[0].scatter(inh_spike_times, inh_spike_ids + NUM_EXCITATORY, s=0.01)
 
 # Plot excitatory rates on second axis
 exc_rate = np.histogram(exc_spike_times, bins=rate_bins)[0]
-axes[1].plot(rate_bin_centres, exc_rate * (1000.0 / bin_size) * (1.0 / exc_num))
+axes[1].plot(rate_bin_centres, exc_rate * (1000.0 / bin_size) * (1.0 / NUM_EXCITATORY))
 
 # Plot inhibitory rates on third axis
 inh_rate = np.histogram(inh_spike_times, bins=rate_bins)[0]
-axes[2].plot(rate_bin_centres, inh_rate * (1000.0 / bin_size) * (1.0 / exc_num))
+axes[2].plot(rate_bin_centres, inh_rate * (1000.0 / bin_size) * (1.0 / NUM_INHIBITORY))
 
 # Label axes
 axes[0].set_ylabel("Neuron ID")
@@ -218,4 +226,3 @@ axes[2].set_ylabel("Inhibitory rate [Hz]")
 axes[2].set_xlabel("Time [ms]");
 
 plt.savefig("wave.png")
-
